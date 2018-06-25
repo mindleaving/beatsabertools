ed25609111f808a44db1294ae54d3463a23bfb13
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-06-25 20:19:00 +0200

    Fix of unit test

--- BeatSaberSongGeneratorTest/BeatDetectorTrainingRunner.cs
+++ BeatSaberSongGeneratorTest/BeatDetectorTrainingRunner.cs
@@ -13,8 +13,8 @@ namespace BeatSabeSonglGeneratorTest
         {
             var songFilePath = @"C:\Users\Jan\Music\Dimitri Vegas - Ocarina.mp3";
             var sut = new BeatDetector();
-            var signal = AudioSampleReader.ReadMonoSamples(songFilePath, out var sampleRate);
-            var beatDetectorResult = sut.DetectBeats(signal, sampleRate);
+            var signal = AudioSampleReader.ReadMonoSamples(songFilePath, out var sampleRate, out var songLength);
+            var beatDetectorResult = sut.DetectBeats(signal, sampleRate, songLength);
             Console.WriteLine($@"BPM: {beatDetectorResult.BeatsPerMinute:F0}");
             foreach (var beat in beatDetectorResult.DetectedBeats)
             {
